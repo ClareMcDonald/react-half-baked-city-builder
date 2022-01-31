ec3965e8880d39d0683723e7072eead4a846bbf9
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-31 12:56:17 -0800

    filled in castleDropdown and rendered it in app.js

--- src/App.js
+++ src/App.js
@@ -34,10 +34,12 @@ function App() {
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
         <section className='dropdowns'>
           
-          {/*  // render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. */}
+          {/*  render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. */}
           <SkylineDropdown setSkylineId={setSkylineId} />
           <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
-          {/* // In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state */}
+          <CastleDropdown setCastleId={setCastleId} />
+
+          {/* In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state */}
           
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
